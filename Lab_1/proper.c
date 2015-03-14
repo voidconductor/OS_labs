4c136beecdfaef60f8a9fa11e79f5b4eb3468b89
voidconductor <Protodwarf@gmail.com>
2015-03-14 12:43:29 +0300

    Добавлены структуры, чтобы были, вспомнил как с ними работать, очень весело xD

--- Lab_1/proper.c
+++ Lab_1/proper.c
@@ -5,9 +5,21 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+struct keeper
+{
+    char *filename;
+    int filetype;
+    int id;
+};
+
 int main(int argc, char** argv)
 {
-struct stat info;
+int i,j;
+struct keeper old[128];
+int size_prev;
+struct keeper fresh[128];
+int size_curr;
+
 DIR *dir;
 struct dirent *entry;
 
@@ -15,12 +27,30 @@ dir = opendir("/home/protodwarf/lolfolder");
 if (!dir) {
         perror("diropen");
         exit(1);
-    };
-
+    }
+/*
     while ( (entry = readdir(dir)) != NULL) {
-        printf("%s [%d]\n",
-            entry->d_name, entry->d_type);
+        printf("%d - %s [%d]\n",
+            entry->d_ino,entry->d_name, entry->d_type);
     };
+*/
+
+    i=0;
+    while ( (entry = readdir(dir)) != NULL) {
+        fresh[i].filename = entry->d_name;
+        fresh[i].id = entry->d_ino;
+        fresh[i].filetype = entry->d_type;
+        i++;
+    }
+    size_curr = i;
+
+    for(j = 0; j < size_curr; j++)
+    {
+        old[j]=fresh[j];
+    }
+    size_prev = size_curr;
+
+    printf("%d \n", size_curr);
 
 return 0;
 }
