9332dda7b7eb13c989973066ea3b2d9560b7dc33
voidconductor <Protodwarf@gmail.com>
2015-04-22 21:27:06 +0300

    execvp rulez!

--- Lab_3/terminal.c
+++ Lab_3/terminal.c
@@ -1,51 +1,86 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char** argv)
+#define MAX_COMMAND_LENGTH 100
+#define MAX_NUMBER_OF_PARAMS 10
+
+// Разделение введённой команды на аргументы
+void parseCmd(char* cmd, char** params)
 {
-    pid_t pid;
-    char *message;
-    int n;
-    int exit_code;
-
-    printf("fork program starting\n");
-    pid = fork();
-    switch(pid)
-    {
-        case -1:
-            perror("fork failed");
-            exit(1);
-        case  0:
-            message = "This is the child";
-            n = 5;
-            exit_code = 37;
-            break;
-        default :
-            message = "This is the parent";
-            n = 3;
-            exit_code = 0;
-            break;
-    }
-
-    for(; n > 0; n--) {
-        puts(message);
-        sleep(1);
-    }
-
-    if (pid != 0) {
-        int stat_val;
-        pid_t child_pid;
-
-        child_pid = wait(&stat_val);
-
-        printf("Child has finished: PID = %d\n", child_pid);
-        if(WIFEXITED(stat_val))
-            printf("Child exited with code %d\n", WEXITSTATUS(stat_val));
-        else
-            printf("Child terminated abnormally\n");
-    }
-exit(exit_code);
+int i;
+for(i = 0; i < MAX_NUMBER_OF_PARAMS; i++)
+{
+    params[i] = strsep(&cmd, " ");
+    if(params[i] == NULL) break;
 }
+}
+
+
+int executeCmd(char** params)
+{
+// Fork
+pid_t pid = fork();
+
+if (pid == -1)
+{
+    char* error = strerror(errno);
+    printf("fork: %s\n", error);
+    return 1;
+}
+
+// Child
+else if (pid == 0)
+{
+    // Execute
+    execvp(params[0], params);
+
+    // Error
+    char* error = strerror(errno);
+    printf("shell: %s: %s\n", params[0], error);
+    return 0;
+}
+
+// Parent
+else
+{
+    // Wait for child
+    int childStatus;
+    waitpid(pid, &childStatus, 0);
+    return 1;
+}
+}
+
+int main()
+{
+char cmd[MAX_COMMAND_LENGTH + 1];
+char* params[MAX_NUMBER_OF_PARAMS + 1];
+
+while(1)
+{
+    // Приглашение.
+    printf("pew-pew>");
+
+    // Считаем команду.
+    if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
+
+    // Удаляем символы перхода если есть.
+    if(cmd[strlen(cmd)-1] == '\n')
+    {
+        cmd[strlen(cmd)-1] = '\0';
+    }
+    // Разбивка ввдённой команды.
+    parseCmd(cmd, params);
+
+    // Если выход, то выход.
+    if(strcmp(params[0], "exit") == 0) break;
+
+    // Выполнение!
+    if(executeCmd(params) == 0) break;
+}
+return 0;
+}
+
+
